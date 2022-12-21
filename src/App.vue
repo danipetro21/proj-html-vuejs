aece1285d4c07a74791339ea943423d09b12039e
danipetro21 <idany346@gmail.com>
2022-12-21 17:46:45 +0100

    add new components and style to project

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,37 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<script>
+//importo tutti i componenti utili alla creazione della pagina
+import AppFooter from './components/AppFooter.vue';
+import AppHeader from './components/AppHeader.vue';
+import Service from './components/Service.vue';
+import WorkWiUs from './components/WorkWiUs.vue';
+
+export default {
+  //definisco i componenti che utilizzerò
+  components: {
+    AppFooter,
+    AppHeader,
+    Service,
+    WorkWiUs
+  }
+}
+
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+
+<AppHeader />
+
+<main>
+
+  <Service />
+
+  <WorkWiUs />
+
+</main>
+
+<AppFooter/>
+
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+<style lang="scss" scoped>
 </style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,25 @@
+<script>
+//importo tutti i componenti utili alla creazione della pagina
+import NavBar from './NavBar.vue';
+import JumboTron from './JumboTron.vue';
+
+
+export default {
+  //definisco i componenti che utilizzerò
+  components: {
+    NavBar,
+    JumboTron
+  }
+}
+
+</script>
+
+<template>
+
+    
+
+</template>
+
+<style lang="scss" scoped>
+@use '../style/partials/variables' as *
+</style>
