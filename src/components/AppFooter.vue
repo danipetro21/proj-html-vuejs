624e3ef8e1e5b47ab92e33b52375aa71e0d9bbce
danipetro21 <idany346@gmail.com>
2022-12-22 17:12:37 +0100

    add component footer

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -3,12 +3,168 @@
 </script>
 
 <template>
+    <section id="footer">
+
+        <div class="footer-top">
+
+            <div class="card">
+                <img src="/img/h-2-blog-img-1.jpg" alt="">
+                <div class="calendar">
+                    <span class="number">07</span>
+                    <span class="month">MAY '19</span>
+                </div>
+                <h1>This way, loves: a detailed guide <br> through new desing</h1>
+                <span class="signature"><i>by Emily Fields</i></span>
+
+            </div>
+            <div class="card">
+                <img src="/img/h-2-blog-img-2.jpg" alt="">
+                <div class="calendar">
+                    <span class="number">07</span>
+                    <span class="month">MAY '19</span>
+                </div>
+                <h1>I try to give people a different way <br> of looking art</h1>
+                <span class="signature"><i>by Emily Fields</i></span>
+
+            </div>
+            <div class="card">
+                <img src="/img/h-2-blog-img-3.jpg" alt="">
+                <div class="calendar">
+                    <span class="number">07</span>
+                    <span class="month">MAY '19</span>
+                </div>
+                <h1>Introduce Richard Laperrière of <br> those amazing features</h1>
+                <span class="signature"><i>by Emily Fields</i></span>
+            </div>
+
+        </div>
 
 
+        <div class="footer-bottom">
+
+
+            <div class="footer-box">
+
+                <div class="logo">
+                    <img src="/img/logo-sidearea-1.png" alt="">
+                    <span>Let's Get Creative</span>
+                </div>
+
+                <div class="contact">
+                    <span>maree.qode@gmail.com</span>
+                    <span>+44645 321 789</span>
+
+                </div>
+
+                <div class="location">
+                    <span>Avenue d'Aiferghem 10</span>
+                    <span>1040 Brussels, Belgium</span>
+
+
+                </div>
+
+                <div class="social">
+                    <span>Stay in touch with us</span>
+                    <ul>
+                        <li><a href=""><i class="fa-brands fa-twitter"></i></a></li>
+                        <li><a href=""><i class="fa-brands fa-pinterest-p"></i></a></li>
+                        <li><a href=""><i class="fa-brands fa-facebook-f"></i></a></li>
+                        <li><a href=""><i class="fa-brands fa-linkedin-in"></i></a></li>
+                    </ul>
+
+                </div>
+            </div>
+        </div>
+
+    </section>
 </template>
 
 <style lang="scss" scoped>
+@use '../style/partials/variables' as *;
+
+#footer {
+    width: 100%;
+}
+
+.footer-top {
+    width: 60%;
+    display: flex;
+    gap: 20px;
+    margin: 100px auto;
+}
+
+.signature {
+    font-size: 0.8rem;
+
+}
+
+.card {
+    width: calc(100%/3);
+    position: relative;
+
+    h1 {
+        margin: 20px 0;
+        font-size: 1.3rem;
+    }
 
 
 
+    img {
+        width: 100%;
+    }
+}
+
+.calendar {
+    text-align: center;
+    width: 70px;
+    display: flex;
+    flex-direction: column;
+    position: absolute;
+    top: -15px;
+    left: 15px;
+}
+
+.number {
+    background-color: $calendar_number;
+    padding: 10px 3px;
+    font-size: 30px;
+    font-weight: 900;
+}
+
+.month {
+    background-color: $calendar_month;
+    padding: 5px 3px;
+    font-size: 12px;
+}
+
+.footer-bottom {
+    width: 100%;
+    background-color: $footer;
+    padding: 60px 0;
+}
+
+.footer-box {
+    width: 60%;
+    margin: 0 auto;
+    display: flex;
+    justify-content: space-between;
+    
+    a{
+        color: black;
+    }
+    span {
+        display: block;
+        margin: 15px 0;
+    }
+
+    ul {
+        display: flex;
+        gap: 20px;
+        justify-content: space-between;
+    }
+
+    li {
+        list-style: none;
+    }
+}
 </style>
--- src/components/WorkWiUs.vue
+++ src/components/WorkWiUs.vue
@@ -7,19 +7,169 @@
 
     <section id="working-process">
 
-        <h1>Working <i><b>process</b></i></h1>
+        <div class="title">
+            <h1>Working <i><b>process</b></i></h1>
+        </div>
+
+        <div class="round-box">
+
+            <div class="container">
+                <div class="round">
+                    <a id="bulb"><i class="fa-solid fa-lightbulb"></i></a>
+                </div>
+
+                <div class="info">
+                    <h4>First theres an idea</h4>
+                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
+                </div>
+            </div>
+            <div class="container">
+                <div class="round">
+                    <a id="comment"><i class="fa-regular fa-comment"></i></a>
+                </div>
+                <div class="info">
+                    <h4>Then we talk about</h4>
+                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
+                </div>
+            </div>
+
+            <div class="container">
+                <div class="round">
+                    <a id="cloud"><i class="fa-solid fa-cloud"></i></a>
+                </div>
+                <div class="info">
+                    <h4>And we think about</h4>
+                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
+                </div>
+            </div>
+
+            <div class="container">
+                <div class="round">
+                    <a id="pencil"><i class="fa-solid fa-pencil"></i></a>
+                </div>
+                <div class="info">
+                    <h4>So we draw along</h4>
+                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
+                </div>
+            </div>
+        </div>
 
     </section>
 
 
     <section id="work-with-us">
 
-
-
+        <h1>Want to work with us? Send us <i><b>a message</b></i></h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur molestiae quasi autem alias facere saepe dolores officia sit. Qui explicabo enim magnam molestiae aspernatur, voluptatem officiis nostrum hic! Similique, rem!</p>
+        <a href="#">CONTACT</a>
     </section>
 
 </template>
 
 <style lang="scss" scoped>
+@use '../style/partials/variables' as *;
 
+h1 {
+    font-size: 4rem;
+    font-weight: 200;
+}
+a{
+    text-decoration: none;
+}
+p {
+        color: $p_color;
+    }
+
+#working-process {
+    max-width: 1200px;
+    margin: 0 auto;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+}
+
+.title {
+    margin-top: 100px;
+
+
+}
+
+.round-box {
+    width: 100%;
+    display: flex;
+    justify-content: space-between;
+    background-image: url("/svg/svg-3.svg");
+    background-size: 83%;
+    background-repeat: no-repeat;
+    background-position: 100px 50px;
+    margin: 100px;
+
+}
+
+//colori icone
+
+#bulb {
+    color: $round_bulb;
+}
+
+#comment {
+    color: $round_commment;
+}
+
+#cloud {
+    color: $round_cloud;
+}
+
+#pencil {
+    color: $round_pencil;
+}
+
+.round {
+
+    display: flex;
+    justify-content: center;
+
+    a {
+        background-color: $background_rond;
+        padding: 50px;
+        border-radius: 50%;
+        font-size: 3rem;
+        margin: 30px 0;
+    }
+
+}
+
+.info {
+    text-align: center;
+    width: 250px;
+
+
+}
+
+#work-with-us{
+    text-align: center;
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+    gap: 50px;
+    align-items: center;
+    height: 500px;
+    background-image: url("img/h-2-background-1.jpg");
+
+    p{
+        width: 80%;
+    }
+
+    a{
+        
+        border: transparent;
+        cursor: pointer;
+        background-color: $btn_contact;
+        padding: 15px 50px;
+        text-align: center;
+        color: black;
+    
+    }
+}
 </style>
